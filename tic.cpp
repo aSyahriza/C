ca1dd2f9fe806022f0f6ba8f387780a90483e8c4
U-BSD\456806 <456806@SHS-DGCSCS2.beaverton.k12.or.us>
2022-10-11 11:31:22 -0700

    not-so-usefull-commit-message

--- tic.cpp
+++ tic.cpp
@@ -1,16 +1,31 @@
+/*
+* 
+* Author: Allam Syahriza  
+* Date: 10/11/2022
+* This programs simulates a two person game of TicTacToe in C++,
+* keeps tracks of wins/ties, and allows the user to play again.
+*
+*/
+
+
+
+
 
 #include <iostream>
 #include <cstring>
 using namespace std;
 
 int printBoard(char board[3][3]){
-  cout << "\t" << "A" << "\t" << "B" << "\t" << "C" << endl;
+  // Prints grid letters and numbers
+  cout << "\n" << "\n" << endl;
+  cout << "\t" << "A" << "\t" << "B" << "\t" << "C" << "\n" <<endl;
   for (int row = 0; row < 3; row++){
     cout << row + 1;
       for (int column = 0; column < 3; column ++){
+        // Prints board contents
         cout << "\t" << board[row][column];
       }
-      cout << "\n";
+      cout << "\n"<<"\n";
   }
 
   return 0;
@@ -18,6 +33,20 @@ int printBoard(char board[3][3]){
   
   
 }
+
+
+bool checkTie(char board[3][3]){
+  // Checks if every square is occupied
+    for (int row = 0; row < 3; row++){
+      for (int column = 0; column < 3; column++){
+        if (board[row][column] == ' '){
+          return false;
+        }
+      }
+    }
+    // If every square is occupied with no winner, it is a tie
+    return true;
+  }
 
 bool checkWin(char player, char board[3][3]){
     if (board[0][0] == player && board[0][1] == player && board[0][2] == player){ // 1st row
@@ -54,19 +83,21 @@ bool checkPlay(char board[3][3]){
   char input;
   cout << "Do you want to play again? (y/n)" << endl;
   cin >> input;
+  // Makes sure user enters only 'y' or 'n'
   while(input != 'y' && input != 'n'){
 
     cout << "Must enter 'y' or 'n'" << endl;
     cin >> input;
 
   }
+  
   if(input == 'y'){
+    // Resets the board
     for (int row = 0; row < 3; row++){
       for (int column = 0; column < 3; column++){
-        board[row][column] = 'S';
+        board[row][column] = ' ';
       }
     }
-    
     return true;
   }
   else{
@@ -88,73 +119,100 @@ int main(){
   char tempRow;
   char tempColumn;
   bool stillPlaying = true;
-  
+  int xWins = 0;
+  int oWins = 0;
+  int ties = 0;
+
+  // Sets the board blank
   for (int row = 0; row < 3; row++){
 
     for (int column = 0; column < 3; column ++){
-      board[row][column] = 'S';
+      board[row][column] = ' ';
       }
   }
+
   while(stillPlaying){
-    while (checkWin('X',board) == false && checkWin('O',board) == false /*&& checkTie() == false*/) {  
+    // Will ask the user for input as long as no winner or tie has occured
+    while (checkWin('X',board) == false && checkWin('O',board) == false && checkTie(board) == false) {  
       printBoard(board);
 
 
-
+      // Ensures user only enters valid rows
       cout << "What row will you make your move?" << endl;
       cin >> tempRow;
-
-
-
       while((tempRow < 49) || (tempRow > 51)){
-
-	cout << "Row entered is invalid!" << endl; 
-	cout << "What row will you make your move?" << endl;
-	cin >> tempRow;
-
+	cout << "Row entered is invalid!" << endl;
+        cout << "What row will you make your move?" << endl;
+        cin >> tempRow;
       }
 
+      // Ensures user only enters valid columns
       cout << "What column will you make your move?" << endl;
       cin >> tempColumn;
-
       while((tempColumn < 65 || tempColumn > 67) && (tempColumn < 97 || tempColumn > 99)){
-	cout << "Column entered is invalid:" << endl; 
-	cout << "What column will you make your move?" << endl;
-	cin >> tempColumn;
+	      cout << "Column entered is invalid:" << endl; 
+	      cout << "What column will you make your move?" << endl;
+	      cin >> tempColumn;
       }
 
+      // Converts the users entered characters to indices for the array
       row = tempRow - 49;
-      column = tempColumn - 65; 
+      if(tempColumn < 68)
+        column = tempColumn - 65;
+      else{
+        column = tempColumn - 97;
+      }
+      
 
-
-      if (board[row][column] == 'S'){
-	if (X_TURN){
-	  board[row][column] = 'X';
-	  X_TURN = false;
-
-
-
-	}
-	else {
-	  board[row][column] = 'O';
-	  X_TURN = true;            
-
-	}
+      // If the designated square is empty, places piece
+      if (board[row][column] == ' '){
+	      if (X_TURN){
+	        board[row][column] = 'X';
+	        X_TURN = false;
+        }
+	      else {
+	        board[row][column] = 'O';
+	        X_TURN = true;            
+        }
+      }
+      // If the square is occupied, has the user choose another square.
+      else{
+        cout << "That spot is occupied! Choose another spot."<< endl;
+        continue;
       }
     }
-
+    printBoard(board);
+    cout << "\n";
+    // Checks if X has won
     if(checkWin('X',board) == true){
-      cout << "X WINS!!!!!!" << endl;  
+      cout << "X WINS!!" << endl;  
+      xWins+=1;
     }
-
+    // Checks if O has won
     else if (checkWin('O',board) == true){
-      cout << "O WINS!!!!!!!!!!" << endl;
+      cout << "O WINS!!" << endl;
+      oWins+=1;
     }
-
-    /*else if (checkTie() == true){
-
-      }*/
-    stillPlaying = checkPlay(board);
+    // Checks if there is a tie
+    else if (checkTie(board) == true){
+      cout << "TIE!!" << endl;
+      ties+=1;
+    }
+    // Prints out the running tallies
+    cout << "Total X Wins: " << xWins << endl;
+    cout << "Total O Wins: " << oWins << endl;
+    cout << "Total Ties: " << ties << endl;
+    
+    
+    // Asks the user if they want to play again 
+    if(checkPlay(board)){
+      stillPlaying = true;
+      X_TURN = true;
+      
+    }
+    else{
+      stillPlaying = false;
+    }
   }
 
   return 0;
